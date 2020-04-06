0b1976a91ec56988e70d28fe6ed0acfd555922ee
Lewis Campbell <lewismcampbell@outlook.com>
2020-04-06 19:16:24 +0000

    deploy the bundle

---
+++ precache-manifest.a87448e08bc0b22fdb5ac38564977615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83405d2d9ce6ffc26220ba2302f3555c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1efbc5d192496612d07",
+    "url": "/static/css/main.9a76ef87.chunk.css"
+  },
+  {
+    "revision": "cde3c7f41c48eb8c059c",
+    "url": "/static/js/2.37c2458d.chunk.js"
+  },
+  {
+    "revision": "de1ff70e117aea440981577109a550f3",
+    "url": "/static/js/2.37c2458d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1efbc5d192496612d07",
+    "url": "/static/js/main.6e366faa.chunk.js"
+  },
+  {
+    "revision": "52f139e4d2d601cecba6",
+    "url": "/static/js/runtime-main.a947b23f.js"
+  }
+]);
